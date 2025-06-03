c683a32a55eb52b3985b2dadadad46d55e32f433
Evgeniy.Gorbunov <qw_msk@list.ru>
2025-06-03 17:59:55 +0300

    init eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+// import js from "@eslint/js";
+// import globals from "globals";
+// import { defineConfig } from "eslint/config";
+
+
+// export default defineConfig([
+//   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+//   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+// ]);
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  // Базовые рекомендуемые правила от ESLint
+  js.configs.recommended,
+
+  {
+    // Файлы, к которым применяется эта конфигурация
+    files: ['**/*.{js,mjs,cjs}'],
+
+    // Глобальные переменные (браузер + Node.js)
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+
+    // Можно добавить/переопределить правила
+    rules: {
+      semi: ['error', 'never'],
+      'no-console': ['warn'],
+    },
+  },
+];
