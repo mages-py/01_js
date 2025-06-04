9f15ec9b6ae9ffd8c74593df28a229d451d6d70e
Evgeniy.Gorbunov <qw_msk@list.ru>
2025-06-04 09:23:41 +0300

    Доработки

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,8 +8,8 @@
 //   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
 //   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
 // ]);
-import js from '@eslint/js';
-import globals from 'globals';
+import js from '@eslint/js'
+import globals from 'globals'
 
 export default [
   js.configs.recommended,
@@ -26,6 +26,8 @@ export default [
     rules: {
       semi: ['error', 'never'],
       'no-console': ['warn'],
+      'no-unused-vars': ['warn'],
+      'no-multiple-empty-lines': ['warn'],
     },
   },
-];
+]
